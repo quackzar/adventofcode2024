e04c13e925ad6f0ffecf1ccd6431c9664611a207
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2024-12-05 21:47:20 +0100

    part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,7 +2,12 @@
 #![feature(coroutine_trait)]
 #![feature(stmt_expr_attributes)]
 
-use std::{fmt::Display, ops::{Coroutine, CoroutineState, Index}, pin::Pin};
+use std::{
+    fmt::Display,
+    isize,
+    ops::{Coroutine, CoroutineState, Index},
+    pin::Pin,
+};
 
 fn main() {
     use std::io::Read;
@@ -15,20 +20,17 @@ fn main() {
     // println!("Solution 2: {res}");
 }
 
-fn new_parser() -> impl Coroutine<char, Yield = (), Return = ()>{
-    #[coroutine] |_: char| {
-        'main: loop {
-            for k in "XMAS".chars() {
-                if (yield) != k {
-                    continue 'main;
-                };
-            }
-            return;
+fn new_parser() -> impl Coroutine<char, Yield = (), Return = ()> {
+    #[coroutine]
+    |_: char| 'main: loop {
+        for k in "XMAS".chars() {
+            if (yield) != k {
+                continue 'main;
+            };
         }
+        return;
     }
 }
-
-
 
 struct Matrix {
     data: Vec<char>,
@@ -39,27 +41,40 @@ struct Matrix {
 impl Matrix {
     fn from_input(input: &str) -> Self {
         let mut data = Vec::new();
-        let rows = input.lines().count();
-        let cols = (input.len()) / rows;
+        let mut rows = 0;
+        let mut cols= 0;
 
         // could just copy the string, might need to handle line endings.
         for line in input.lines() {
+            rows += 1;
             for c in line.chars() {
-                data.push(c)
+                data.push(c);
+                cols += 1;
             }
         }
-        Self {
-            data,
-            rows,
-            cols
-        }
+
+        let cols = cols / rows;
+        Self { data, rows, cols }
+    }
+
+    fn get(&self, i: isize, j: isize) -> Option<char> {
+        if i < 0 {
+            return None;
+        };
+        if j < 0 {
+            return None;
+        };
+
+        let n = i as usize * self.rows;
+        let m = n + self.cols;
+        self.data.get(n..m)?.get(j as usize).cloned()
     }
 }
 
 impl Display for Matrix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for i in 0..self.cols {
-            for j in 0..self.rows {
+        for i in 0..self.rows {
+            for j in 0..self.cols {
                 let c = self[i][j];
                 write!(f, "{c}")?;
             }
@@ -79,98 +94,79 @@ impl Index<usize> for Matrix {
     }
 }
 
-
 fn solve_1(input: &str) -> u32 {
     let matrix = Matrix::from_input(input);
     let mut found = 0;
     // forwards
     for i in 0..matrix.rows {
-        let mut parser = new_parser();
         for j in 0..matrix.cols {
-            let c = matrix[i][j];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-    // backwards
-    for i in 0..matrix.rows {
-        let mut parser = new_parser();
-        for j in (0..matrix.cols).rev() {
-            let c = matrix[i][j];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-    // down
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in 0..matrix.rows {
-            let c = matrix[i][j];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-    // up
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in (0..matrix.rows).rev() {
-            let c = matrix[i][j];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-
-    // \ forward?
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in 0..(matrix.cols - j) {
-            let c = matrix[i][j+i];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-
-    // \ backward?
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in (0..matrix.cols).rev() {
-            let c = matrix[i][j+i];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-
-    // / forward?
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in 0..matrix.cols {
-            let c = matrix[j+i][i];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
-            }
-        }
-    }
-    // / backward?
-    for j in 0..matrix.cols {
-        let mut parser = new_parser();
-        for i in (0..matrix.cols).rev() {
-            let c = matrix[j+i][i];
-            if let CoroutineState::Complete(_) = Pin::new(&mut parser).resume(c) {
-                found += 1;
-                parser = new_parser();
+            let mut forward = new_parser();
+            Pin::new(&mut forward).resume('_');
+            let mut backward = new_parser();
+            Pin::new(&mut backward).resume('_');
+            let mut up = new_parser();
+            Pin::new(&mut up).resume('_');
+            let mut down = new_parser();
+            Pin::new(&mut down).resume('_');
+            let mut diag0 = new_parser();
+            Pin::new(&mut diag0).resume('_');
+            let mut diag1 = new_parser();
+            Pin::new(&mut diag1).resume('_');
+            let mut diag2 = new_parser();
+            Pin::new(&mut diag2).resume('_');
+            let mut diag3 = new_parser();
+            Pin::new(&mut diag3).resume('_');
+            let i = i as isize;
+            let j = j as isize;
+            for k in 0..4 {
+                if let Some(c) = matrix.get(i, j + k) {
+                    match Pin::new(&mut forward).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i, j - k) {
+                    match Pin::new(&mut backward).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i + k, j) {
+                    match Pin::new(&mut down).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i - k, j) {
+                    match Pin::new(&mut up).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i + k, j + k) {
+                    match Pin::new(&mut diag0).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i + k, j - k) {
+                    match Pin::new(&mut diag1).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i - k, j + k) {
+                    match Pin::new(&mut diag2).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
+                if let Some(c) = matrix.get(i - k, j - k) {
+                    match Pin::new(&mut diag3).resume(c) {
+                        CoroutineState::Yielded(_) => (),
+                        CoroutineState::Complete(_) => found += 1,
+                    }
+                };
             }
         }
     }
@@ -179,8 +175,28 @@ fn solve_1(input: &str) -> u32 {
 
 
 
+fn solve_2(input: &str) -> u32 {
+    let matrix = Matrix::from_input(input);
+    let mut found = 0;
+
+    for i in 0..matrix.rows {
+        for j in 0..matrix.cols {
+            let mut parser = new_parser();
+            let i = i as isize;
+            let j = j as isize;
+
+
+
+
+
+        }
+    }
+
+    found
+}
+
 #[cfg(test)]
-const INPUT : &str = "\
+const INPUT: &str = "\
 MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -195,4 +211,9 @@ MXMXAXMASX";
 #[test]
 fn test_1() {
     assert_eq!(solve_1(INPUT), 18);
+}
+
+#[test]
+fn test_2() {
+    assert_eq!(solve_2(INPUT), 9);
 }
