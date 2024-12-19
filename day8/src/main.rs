717727efd6fcd21f65c0fe015b1c6aab37ed25d5
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2024-12-19 22:42:25 +0100

    day8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::{array::from_fn, collections::{BTreeMap, BTreeSet}, iter};
 
 use itertools::Itertools;
 
@@ -8,8 +8,8 @@ fn main() {
     std::io::stdin().read_to_string(&mut input).unwrap();
     let res = solve_1(&input);
     println!("Solution 1: {res}");
-    // let res = solve_2(&input);
-    // println!("Solution 2: {res}");
+    let res = solve_2(&input);
+    println!("Solution 2: {res}");
 }
 
 fn solve_1(input: &str) -> u32 {
@@ -33,7 +33,56 @@ fn solve_1(input: &str) -> u32 {
                 .flat_map(|(a, b)| antinodes(a, b))
         })
         .unique()
-        .filter(|(x, y)| (0..n).contains(x) && (0..m).contains(y))
+        .filter(|(x, y)| (0..n+1).contains(x) && (0..m+1).contains(y))
+        .collect();
+
+    for loc in locs.iter() {
+        *map.get_mut(loc).unwrap() = '#';
+    }
+
+    locs.len() as u32
+}
+
+
+fn solve_2(input: &str) -> u32 {
+    let (mut map, freqs) = parse(input);
+    let &(n, m) = map.keys().max().unwrap();
+
+    let antinodes =|a: Position, b: Position| {
+        let map = &map;
+        let (x0, y0) = a;
+        let (x1, y1) = b;
+        let dx = x0 - x1;
+        let dy = y0 - y1;
+        iter::successors(Some((x0, y0)), move |&(x,y)| {
+            let pos = (x - dx, y - dy);
+            if map.contains_key(&pos) {
+                Some(pos)
+            } else {
+                None
+            }
+        }).chain(iter::successors(Some((x1, y1)), move |&(x,y)| {
+            let pos = (x + dx, y + dy);
+            if map.contains_key(&pos) {
+                Some(pos)
+            } else {
+                None
+            }
+        }))
+    };
+
+
+    let locs: Vec<_> = freqs
+        .into_iter()
+        .flat_map(|(_, locs)| {
+            locs.into_iter()
+                .tuple_combinations()
+                .flat_map(|(a, b)| antinodes(a, b))
+        })
+        .unique()
+        .filter(|(x, y)| (0..n+1).contains(x) && (0..m+1).contains(y))
+        //.filter(|pos| map.get(pos).cloned() == Some('.'))
+        // weird that this is a none-rule but ok
         .collect();
 
     for loc in locs.iter() {
@@ -47,8 +96,8 @@ fn solve_1(input: &str) -> u32 {
 
 fn print_map(map: &Map) {
     let &(n, m) = map.keys().max().unwrap();
-    for i in 0..n {
-        for j in 0..m {
+    for j in 0..n+1 {
+        for i in 0..m+1 {
             let c = map.get(&(i, j)).unwrap();
             print!("{c}")
         }
@@ -109,4 +158,10 @@ const EXAMPLE: &str = "\
 #[test]
 fn test_1() {
     assert_eq!(solve_1(EXAMPLE), 14);
+}
+
+
+#[test]
+fn test_2() {
+    assert_eq!(solve_2(EXAMPLE), 34);
 }
